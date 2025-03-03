07bf746e777c51cea2cfe13b8d7e06ed780cbf7e
Jason <ivandomogatskij@gmail.com>
2025-03-03 12:17:27 +0300

    axes labels

--- lab_6/task_10.py
+++ lab_6/task_10.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 E = list(map(float, input('Значения E через пробел : ').split()))
 M = list(map(float, input('Значения M через пробел : ').split()))
 
-
+plt.ylabel('значения E')
+plt.xlabel('значения M')
 
 plt.plot(M, E, 'ro')
 plt.plot(M, E)
